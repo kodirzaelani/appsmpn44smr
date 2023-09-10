3efab5ff46eb87d2890239f87e012c95c7157426
kodirzaelani <laman.kreasi@gmail.com>
2023-09-11 00:11:49 +0800

    update User Model (HasRoles, Uuid, Softdeletes)

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,25 +3,74 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+
+use App\Traits\Uuid;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use SoftDeletes;
+    use HasRoles;
+    use Uuid;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded    = [];
+    protected $primaryKey = 'id';
+
+    public function scopeSearch($query, $term)
+    {
+        $term = "%$term%";
+        $query->where(function ($query) use ($term) {
+            $query->where('name', 'like', $term);
+        });
+    }
+
+    public function getImageUrlAttribute($value)
+    {
+        $imageUrl = "";
+
+        if (!is_null($this->image)) {
+            $directory = config('cms.image.directoryUsers');
+            $imagePath = public_path() . "/{$directory}" . $this->image;
+            if (file_exists($imagePath)) $imageUrl = asset("/{$directory}" . $this->image);
+        }
+
+        return $imageUrl;
+    }
+
+    public function getImageThumbUrlAttribute($value)
+    {
+        $imageThumbUrl = "";
+
+        if (!is_null($this->image)) {
+            $directory = config('cms.image.directoryUsers');
+            $imagePath = public_path() . "/{$directory}/images_thumb/" . $this->image;
+            if (file_exists($imagePath)) $imageThumbUrl = asset("/{$directory}/images_thumb/" . $this->image);
+        }
+
+        return $imageThumbUrl;
+    }
+    public function getStatusLabelAttribute()
+    {
+        //ADAPUN VALUENYA AKAN MENCETAK HTML BERDASARKAN VALUE DARI FIELD STATUS
+        if ($this->status == 0) {
+            return '<span class="badge badge-primary">Inactive</span>';
+        }
+        return '<span class="badge badge-success">Active</span>';
+    }
+    public function getStatusTextAttribute()
+    {
+        //ADAPUN VALUENYA AKAN MENCETAK HTML BERDASARKAN VALUE DARI FIELD STATUS
+        if ($this->status == 0) {
+            return '<span class="fw-bold ml-2">Inactive</span>';
+        }
+        return '<span class="fw-bold">Active</span>';
+    }
 
     /**
      * The attributes that should be hidden for serialization.
