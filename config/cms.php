479ddf9bf37a89008a5ac197b2d64da718078e23
kodirzaelani <laman.kreasi@gmail.com>
2023-09-15 22:47:00 +0800

    add cms config

---
+++ config/cms.php
@@ -0,0 +1,168 @@
+<?php
+return [
+    'image' => [
+        'directoryPosts' => 'uploads/images/posts/',
+        'thumbnailpost' => [
+            'width' => 300,
+            'height' => 200
+        ],
+        'directoryCategorypost' => 'uploads/images/postcategory/',
+        'thumbnailpostcategory' => [
+            'width' => 300,
+            'height' => 200
+        ],
+
+        'directoryPengajiancategory' => 'uploads/images/pengajiancategory/',
+        'thumbnailpengajiancategory' => [
+            'width' => 300,
+            'height' => 200
+        ],
+        'directoryTestimonial' => 'uploads/images/testimonial/',
+        'thumbnailtestimonial' => [
+            'width' => 200,
+            'height' => 300
+        ],
+        'directoryPengajian' => 'uploads/images/pengajian/',
+        'thumbnailpengajian' => [
+            'width' => 300,
+            'height' => 200
+        ],
+        'directorySubCategorypost' => 'uploads/images/postsubcategory/',
+        'thumbnailpostsubcategory' => [
+            'width' => 300,
+            'height' => 200
+        ],
+        'directoryLiteracy' => 'uploads/images/literacy/',
+        'thumbnailliteracy' => [
+            'width' => 200,
+            'height' => 400
+        ],
+        'directoryLiteracycategory' => 'uploads/images/literacycategory/',
+        'thumbnailliteracy' => [
+            'width' => 200,
+            'height' => 300
+        ],
+        'directoryLiteracysubcategory' => 'uploads/images/literacysubcategory/',
+        'thumbnailliteracy' => [
+            'width' => 200,
+            'height' => 300
+        ],
+        'directorySetarticle' => 'uploads/images/setarticle/',
+        'thumbnailsetarticle' => [
+            'width' => 300,
+            'height' => 200
+        ],
+        'directoryPages' => 'uploads/images/pages/',
+        'thumbnailpage' => [
+            'width' => 300,
+            'height' => 200
+        ],
+        'directoryGreetings' => 'uploads/images/greetings/',
+        'thumbnailgreeting' => [
+            'width' => 300,
+            'height' => 200
+        ],
+        'directoryAdvertisements' => 'uploads/images/advertisements/',
+        'thumbnailadvertisement' => [
+            'width' => 200,
+            'height' => 300
+        ],
+        'directorySliders' => 'uploads/images/sliders/',
+        'thumbnailslider' => [
+            'width' => 300,
+            'height' => 200
+        ],
+        'directoryAlbums' => 'uploads/images/albums/',
+        'thumbnailalbum' => [
+            'width' => 300,
+            'height' => 200
+        ],
+        'directoryVideocategories' => 'uploads/images/videocategories/',
+        'thumbnailcategoryvideo' => [
+            'width' => 300,
+            'height' => 200
+        ],
+        'directoryVideos' => 'uploads/images/videos/',
+        'thumbnailvideo' => [
+            'width' => 300,
+            'height' => 200
+        ],
+        'directoryVideocategories' => 'uploads/images/videocategories/',
+        'thumbnailvideocategories' => [
+            'width' => 300,
+            'height' => 200
+        ],
+        'directoryCourses' => 'uploads/images/courses/',
+        'thumbnailcourse' => [
+            'width' => 300,
+            'height' => 200
+        ],
+        'directoryScreenshoot' => 'uploads/images/screenshoots/',
+        'thumbnailscreenshoot' => [
+            'width' => 300,
+            'height' => 200
+        ],
+        'directoryTools' => 'uploads/images/tools/',
+        'thumbnailtool' => [
+            'width' => 300,
+            'height' => 200
+        ],
+        'directorySections' => 'uploads/images/sections/',
+        'thumbnailsection' => [
+            'width' => 300,
+            'height' => 200
+        ],
+        'directoryAgenda' => 'uploads/images/agenda/',
+        'thumbnailagenda' => [
+            'width' => 300,
+            'height' => 200
+        ],
+        'directoryWidget' => 'uploads/images/widget/',
+        'thumbnailwidget' => [
+            'width' => 200,
+            'height' => 200
+        ],
+        'directoryTamu' => 'uploads/images/visitor/',
+        'thumbnailtamu' => [
+            'width' => 200,
+            'height' => 300
+        ],
+        'directoryProsesTamu' => 'uploads/images/prosesvisitor/',
+        'thumbnailtamu' => [
+            'width' => 200,
+            'height' => 300
+        ],
+        'directoryFoto' => 'uploads/images/fotos/',
+        'thumbnailfoto' => [
+            'width' => 300,
+            'height' => 200
+        ],
+        'directoryUsers' => 'uploads/images/users/',
+        'thumbnailuser' => [
+            'width' => 300,
+            'height' => 200
+        ],
+        'directoryDownloads' => 'uploads/downloads/files/',
+        'thumbnailuser' => [
+            'width' => 300,
+            'height' => 200
+        ],
+        'directoryLogo' => 'uploads/images/logo/',
+        'thumbnaillogo' => [
+            'width' => 300,
+            'height' => 300
+        ],
+        'thumbnailfavicon' => [
+            'width' => 16,
+            'height' => 16
+        ]
+    ],
+    'default_mastertag' => 1,
+    'default_masterpostcategory' => 1,
+    'default_masterpostsubcategory' => 1,
+    'default_masterpagecategory' => 1,
+    'default_masterliterasicategory' => 1,
+    'default_masterliterasisubcategory' => 1,
+    'default_masteruser' => 1,
+    'masterpengajiancategory' => 1,
+];
