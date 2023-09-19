e81ec69ee7aabb66fdd4d35d7d4d47ec40c92fb4
kodirzaelani <laman.kreasi@gmail.com>
2023-09-20 06:41:42 +0800

    add model & table Slider

---
+++ app/Models/Slider.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Uuid;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Slider extends Model
+{
+    use HasFactory;
+    use Uuid;
+    protected $table        = 'sliders';
+    protected $primaryKey   = 'id';
+    protected $guarded      = [];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class, 'post_id');
+    }
+
+    public function scopeSearch($query, $term)
+    {
+        $term = "%$term%";
+        $query->where(function ($query) use ($term) {
+            $query->where('title', 'like', $term);
+        });
+    }
+
+    public function getImageUrlAttribute($value)
+    {
+        $imageUrl = "";
+
+        if (!is_null($this->image)) {
+            $directory = config('cms.image.directorySliders');
+            $imagePath = public_path() . "/{$directory}" . $this->image;
+            if (file_exists($imagePath)) $imageUrl = asset("/{$directory}" . $this->image);
+        }
+
+        return $imageUrl;
+    }
+
+    public function getImageThumbUrlAttribute($value)
+    {
+        $imageThumbUrl = "";
+
+        if (!is_null($this->image)) {
+            $directory = config('cms.image.directorySliders');
+            $ext       = substr(strrchr($this->image, '.'), 1);
+            $thumbnail = str_replace(".{$ext}", "_thumb.{$ext}", $this->image);
+            $imagePath = public_path() . "/{$directory}" . $thumbnail;
+            if (file_exists($imagePath)) $imageThumbUrl = asset("/$directory" . $thumbnail);
+        }
+
+        return $imageThumbUrl;
+    }
+
+
+    public function getStatusLabelAttribute()
+    {
+        //ADAPUN VALUENYA AKAN MENCETAK HTML BERDASARKAN VALUE DARI FIELD STATUS
+        if ($this->status == 0) {
+            return '<span class="badge badge-primary">Draft</span>';
+        }
+        return '<span class="badge badge-success">Published</span>';
+    }
+}
---
+++ database/migrations/2023_09_20_063944_create_sliders_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sliders', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->uuid('author_id');
+            $table->string('image');
+            $table->string('title');
+            $table->string('slug');
+            $table->string('short_title')->nullable();
+            $table->string('excerpt')->nullable();
+            $table->uuid('post_id');
+            $table->string('video')->nullable();
+            $table->boolean('show_attribute')->default(false);
+            $table->boolean('status')->default(true);
+            $table->boolean('statusbanner')->default(false);
+            $table->uuid('updated_by')->nullable();
+            $table->uuid('deleted_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->primary('id');
+
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('restrict');
+            $table->foreign('author_id')->references('id')->on('users')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sliders');
+    }
+};
