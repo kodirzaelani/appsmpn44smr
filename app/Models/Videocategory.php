b94489319f0d21b3fb107513f84040895308c4e0
kodirzaelani <laman.kreasi@gmail.com>
2023-09-20 06:43:45 +0800

    add model & table videocategory

---
+++ app/Models/Videocategory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Videocategory extends Model
+{
+    use HasFactory;
+}
