f2590c4fe2664af52bd5a045bb2aa0b4090be4a2
kodirzaelani <laman.kreasi@gmail.com>
2023-09-20 07:01:50 +0800

    add model & table announcement

---
+++ database/migrations/2023_09_20_065857_create_announcements_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('announcements', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->uuid('author_id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('image')->nullable();
+            $table->string('attach')->nullable();
+            $table->text('description');
+            $table->boolean('status');
+            $table->integer('view_count')->default(0);
+            $table->uuid('updated_by')->nullable();
+            $table->uuid('deleted_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->primary('id');
+
+            $table->foreign('author_id')->references('id')->on('users')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('announcements');
+    }
+};
